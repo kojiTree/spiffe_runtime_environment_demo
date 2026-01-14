45ff9b96e535c0a0b8cf12b271992d17ffafd7af
kojiTree <koji@furo-inc.com>
2026-01-14 15:23:29 +0900

    fix: demo

--- demo/server/main.go
+++ demo/server/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"log"
 	"net/http"
 	"os"
 	"time"
+	"net"
 
-	"github.com/spiffe/go-spiffe/v2/spiffetls"
 	"github.com/spiffe/go-spiffe/v2/spiffetls/tlsconfig"
 	"github.com/spiffe/go-spiffe/v2/workloadapi"
 )
@@ -25,37 +27,59 @@ func main() {
 	}
 	log.Printf("Server SVID: %s", svid.ID)
 
-	listener, err := spiffetls.ListenWithMode(
-	    ctx,
-	    "tcp",
-	    listenAddr,
-	    spiffetls.MTLSServerWithSource(tlsconfig.AuthorizeAny(), source),
-	)
+	listener, err := net.Listen("tcp", listenAddr)
 	if err != nil {
-		log.Fatalf("failed to start mTLS listener: %v", err)
+	    log.Fatalf("failed to listen: %v", err)
 	}
 
 	log.Printf("demo-server listening on %s", listenAddr)
 	server := &http.Server{
-		ReadHeaderTimeout: 5 * time.Second,
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			clientID := "unknown"
-			if r.TLS != nil {
-			    if id, err := spiffetls.PeerIDFromConnectionState(*r.TLS); err == nil {
-			        clientID = id.String()
-			    } else {
-			        log.Printf("could not get peer SPIFFE ID: %v", err)
-			    }
-			}
-			log.Printf("Client SPIFFE ID: %s", clientID)
-			w.WriteHeader(http.StatusOK)
-			_, _ = w.Write([]byte("hello from demo-server\n"))
-		}),
+	    ReadHeaderTimeout: 5 * time.Second,
+	    TLSConfig: tlsconfig.MTLSServerConfig(
+	        source, // サーバ証明書/鍵
+	        source, // trust bundle
+	        tlsconfig.AuthorizeAny(), // 後で AuthorizeID にするの推奨
+	    ),
+	    Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	        log.Printf("Client SPIFFE ID: %s", clientSPIFFEID(r.TLS))
+	        w.WriteHeader(http.StatusOK)
+	        _, _ = w.Write([]byte("hello from demo-server\n"))
+	    }),
 	}
 
-	if err := server.Serve(listener); err != nil && err != http.ErrServerClosed {
-		log.Fatalf("server error: %v", err)
+	// certFile/keyFile は TLSConfig 内にあるので空でOK
+	if err := server.ServeTLS(listener, "", ""); err != nil && err != http.ErrServerClosed {
+	    log.Fatalf("server error: %v", err)
 	}
+}
+
+func clientSPIFFEID(state *tls.ConnectionState) string {
+	if state == nil {
+		return "unknown: no TLS state"
+	}
+
+	cert := clientCert(state)
+	if cert == nil {
+		return "unknown: no client certificate"
+	}
+
+	for _, uri := range cert.URIs {
+		if uri != nil && uri.Scheme == "spiffe" {
+			return uri.String()
+		}
+	}
+
+	return "unknown: no SPIFFE ID in client certificate"
+}
+
+func clientCert(state *tls.ConnectionState) *x509.Certificate {
+	if len(state.VerifiedChains) > 0 && len(state.VerifiedChains[0]) > 0 {
+		return state.VerifiedChains[0][0]
+	}
+	if len(state.PeerCertificates) > 0 {
+		return state.PeerCertificates[0]
+	}
+	return nil
 }
 
 func mustFetchSource(ctx context.Context) *workloadapi.X509Source {
